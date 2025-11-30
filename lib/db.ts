abc250815455ad45dd93b59f5f4db273cdf780e4
loversmilsad11 <skynetbilal05@gmail.com>
2025-11-30 14:34:55 +0100

    v4

--- lib/db.ts
+++ lib/db.ts
@@ -5,10 +5,6 @@ import { PrismaClient } from "./generated/prisma";
 const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
 export const prisma =
-    globalForPrisma.prisma || new PrismaClient({
-
-    }
-
-    );
+    globalForPrisma.prisma || new PrismaClient();
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
